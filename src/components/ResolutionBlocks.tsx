2b6a328731e27c99c1b86779cda0acdd5e237508
Wenlei Dai <resca.wen@gmail.com>
2025-02-10 13:00:18 +0200

    Commented out flexbox based layout, added column based.

--- src/components/ResolutionBlocks.tsx
+++ src/components/ResolutionBlocks.tsx
@@ -1,10 +1,13 @@
+import type { Dispatch } from 'react'
 import { borderClasses } from '../utils/utils'
 import { Action } from '../reducers/displayDataReducer'
 import devices from '../data/devices.json'
 
-function Container({ dispatch }: { dispatch: React.Dispatch<Action> }) {
+function Container({ dispatch }: { dispatch: Dispatch<Action> }) {
    return (
-      <div className={`flex flex-wrap`}>
+
+      // column based layout
+      <div className="columns-4">
          {devices.map(devices => (
             <div key={devices.brand} className={`relative pt-3 px-2 pb-2 m-2 ${borderClasses}`}>
                <div className="absolute -top-3.5 left-1/4 transform -translate-x-1/2 bg-white px-1">
@@ -23,6 +26,27 @@ function Container({ dispatch }: { dispatch: React.Dispatch<Action> }) {
             </div>
          ))}
       </div>
+
+      // flexbox based layout
+      // <div className={`flex flex-wrap items-start`}>
+      //    {devices.map(devices => (
+      //       <div key={devices.brand} className={`relative pt-3 px-2 pb-2 m-2 ${borderClasses}`}>
+      //          <div className="absolute -top-3.5 left-1/4 transform -translate-x-1/2 bg-white px-1">
+      //             {devices.brand}
+      //          </div>
+      //          {devices.devices.map(({ resolution: { horizontal, vertical }, diagonal, label }, index) => (
+      //             <div key={index}>
+      //                <span className="text-blue-600 cursor-pointer underline"
+      //                   onClick={() => dispatch({ type: 'SET_ALL', payload: { resolution: { horizontal, vertical }, diagonal } })}
+      //                >
+      //                   {`${horizontal}x${vertical} @ ${diagonal}`}
+      //                </span>
+      //                &nbsp;{`${label}`}
+      //             </div>
+      //          ))}
+      //       </div>
+      //    ))}
+      // </div>
    )
 }
 
