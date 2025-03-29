aa49a4ed8200ec8689d541073c1461ea39628cd6
Wenlei Dai <resca.wen@gmail.com>
2025-03-29 09:56:10 +0200

    Auto lint and preparing for github pages.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-})
+export default defineConfig({ base: "/ppi-calculator/", plugins: [react(), tailwindcss()] })
